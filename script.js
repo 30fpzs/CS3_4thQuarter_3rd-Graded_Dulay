aa6fc7e2d5aef4a93a82fd15ed16185b934d7b9f
30fpzs <b2029mufdulay@pshs.edu.ph>
2026-03-31 21:41:21 +0800

    final commit ! :D

--- script.js
+++ script.js
@@ -2,12 +2,17 @@ function plotPoint(x0, y0, x, y) {
     console.log(x0, y0, x, y) // checks the arguments passed to this function
     document.getElementById("in1").innerHTML = x0 + " " + y0 + " " + x + " " + y;
     const plane = document.getElementById('coordinatePlane');
+    const oldPoint = document.querySelector('.point'); // selects the first element with the class 'point'
+    if(oldPoint)
+    {
+      oldPoint.remove(); // removes the old point from the coordinate plane
+    }
     const point = document.createElement('div');  
     point.className = 'point';
     /* control where to place the div on the screen using left and bottom of position:absolute */
     point.style.left = (x - x0 + 200 - 5) + 'px'; // subtract half the width of the point to center it by changing left css property
     point.style.bottom = (y - y0 + 200 - 5) + 'px'; // subtract half the height of the point to center it by changing bottom css property
-    document.getElementById('coordinatePlane').appendChild(point);
+    plane.appendChild(point); // adds the new point to the coordinate plane
 
     if (x > x0 && y > y0)
       {
@@ -17,7 +22,7 @@ function plotPoint(x0, y0, x, y) {
       {
       document.getElementById("out1").innerHTML = "SO"
       }
-    else if (x > x0 && y < y0)
+    else if (x > x0 && y < y0) 
       {
       document.getElementById("out1").innerHTML = "SE"
       }
